7b448161f6c5a557e5fd46d4173707c15470934c
nguyenvanconggithub <41448225+nguyenvanconggithub@users.noreply.github.com>
2018-10-25 11:54:12 +0700

    Update Program.cs

--- AlertRelax/Program.cs
+++ AlertRelax/Program.cs
@@ -14,6 +14,7 @@ namespace AlertRelax
         [STAThread]
         static void Main()
         {
+            //.
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
